d973c182ab2ae3f567d649181f68a6e223dedc93
j0k3r-PGR <j0k3r.rg2023@gmail.com>
2023-05-29 08:57:14 -0300

    carga de barcos, jugador puede tirar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
+import { useState } from 'react'
 import './App.css'
 import { Tablero } from './Tablero.jsx'
 
 
 function App() {
+  const [ready, setReady] = useState(false)
+  const [className,setClassName] = useState("inicioUser")
+  const [misBarcos, setMisBarcos] = useState([])
+  const [barcosPc, setBarcosPc] = useState([])
 
+  const iniciar = () =>{
+    if(ready) {setClassName("none")}else{alert("debe Cargar Los Barcos")}
+  }
 
+  const actualyReady = () => {
+    setReady(true)
+  }
+
+  const actualyMisBarcos = (prop) => {
+    setMisBarcos(prop)
+    const newBarcosPc = [... barcosPc]
+    let repeat = false
+    let num = 0
+    while (!repeat){
+      num = Math.floor(Math.random() * 81)
+      repeat = barcosPc.every( element => element != num)
+    }
+    newBarcosPc.push(num)
+    setBarcosPc(newBarcosPc)
+  }
 
   return (
     <>
+    <div className={className}>
+      <h1>Seleccione sus barcos</h1>
+      <Tablero select={true} valido={true} actualyReady={actualyReady} seleccion misBarcos={misBarcos} setMisBarcos={actualyMisBarcos} />
+      <button onClick={iniciar}>Iniciar</button>
+    </div>
     <h1>BATTLE GAME</h1>
     <div className='tableros'>
-      <Tablero tittle="PC Table" valido={true} clas=""/>
-      <Tablero tittle="My Table" valido={false} clas="pc"/>
+      <Tablero tittle="PC Table" valido={true} clas="player" juego barcosPc={barcosPc}/>
+      <Tablero tittle="My Table" valido={false} clas="pc" barcosPc={misBarcos}/>
     </div>
     </>
   )
--- src/Tablero.jsx
+++ src/Tablero.jsx
@@ -5,8 +5,18 @@ const AUX = {
     B : "  "
 }
 
-const Box = ({children,className,updateBox,index}) =>{
-    const newClassName = className + (children ? ' blue': '')
+let aciertos = Array(81).fill(false)
+
+const Box = ({children,className,updateBox,index, seleccion,juego,acierto}) =>{
+    const [color,setColor] = useState("")
+
+    let newClassName = className
+    if (seleccion) {
+        newClassName += (children == AUX.A ? ' blue': '')
+    } 
+    if (juego){
+        newClassName += acierto ? (children == AUX.A ? " red" : " blue") : ""
+    }
 
     const hanleClick = () => {
         updateBox(index)
@@ -19,19 +29,39 @@ const Box = ({children,className,updateBox,index}) =>{
     )
 }
 
-
-export function Tablero({tittle,valido ,id, clas}){
+export function Tablero(
+    {tittle,valido ,id, clas,actualyReady,seleccion,juego,misBarcos,setMisBarcos,barcosPc}
+    ){
     const [mapBox, setMapBox] = useState(Array(81).fill(null))
-    const barcosRestantes = 10
-   
+    const [barcos,setBarcos] = useState(10)
+    const [noWin,setNoWin] = useState(true)
     const updateBox = (index) => {
-        if(mapBox[index] || !valido) return
-        const newMapBox = [... mapBox]
-        newMapBox[index] = AUX.A
-        setMapBox(newMapBox)
+        if(mapBox[index] || !valido || !noWin) return
+        if (barcos >= 1 & seleccion){
+            setBarcos(barcos-1)
+            if(barcos===1) actualyReady()
+            const newMapBox = [... mapBox]
+            newMapBox[index] = AUX.A
+            setMapBox(newMapBox)
+            const barc = [... misBarcos]
+            barc.push(index)
+            setMisBarcos(barc)
+        }
+        let win = false
+        if (barcos===0) {
+            setNoWin(false)
+            win = true
+        }
+        if (juego & !win){
+            setBarcos(barcosPc.some(element => element == index)? barcos-1 : barcos)
+            aciertos[index] = true
+            const newMapBox = [... mapBox]
+            newMapBox[index] = AUX.A
+            setMapBox(newMapBox)
+        }
         
-        const tablePC = document.getElementsByClassName("box pc")
-        tablePC[Math.floor(Math.random() * tablePC.length)].className = "box blue"
+        //const tablePC = document.getElementsByClassName("box pc")
+        //tablePC[Math.floor(Math.random() * tablePC.length)].className = "box blue"
     }
 
     return(
@@ -46,9 +76,13 @@ export function Tablero({tittle,valido ,id, clas}){
                 index={index}
                 className={"box "+clas}
                 updateBox = {updateBox}
+                seleccion = {seleccion}
+                juego= {juego}
+                barcosPc = {barcosPc}
                 id = {id}
+                acierto = {aciertos[index]}
                 >
-                    {mapBox[index]}
+                    {barcosPc ? (barcosPc.some(element => index === element)? AUX.A : AUX.B) : mapBox[index]}
                 </Box>
                 )
             } )
@@ -56,7 +90,7 @@ export function Tablero({tittle,valido ,id, clas}){
         </div>
         <div className='info'>
             <div className='restantes'>
-            <label>BARCOS RESTANTES: {barcosRestantes}  </label>
+            <label>BARCOS RESTANTES: {barcos}  </label>
             </div>
         </div>
     </div>
